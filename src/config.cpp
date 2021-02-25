f3f19edc11140450e100d9e89816724883c7958e
Jerry Yin <jerryyin@cs.ubc.ca>
2021-02-24 23:39:41 -0800

    Support center-aligning bar, set highlight colour automatically

--- src/config.cpp
+++ src/config.cpp
@@ -1,1 +1,24 @@
 #include "config.h"
+
+#include <dwmapi.h>
+
+namespace vwtiling {
+
+Config::Config()
+  : height(26)
+  , pad(10)
+  , corner_radius(0) // TODO: Transparency is janky.
+  , label_width(height)
+  , outer_gap(16)
+  , alignment(Alignment::Center) {
+  DWORD active_caption; // AARRGGBB
+  BOOL opaque_blend;
+  if (DwmGetColorizationColor(&active_caption, &opaque_blend) == S_OK) {
+    highlight_color.r = ((active_caption >> 16) & 0xFF) / 255.f;
+    highlight_color.g = ((active_caption >> 8) & 0xFF) / 255.f;
+    highlight_color.b = (active_caption & 0xFF) / 255.f;
+  }
+  inactive_text_color = highlight_color;
+}
+
+} // namespace vwtiling
--- src/config.h
+++ src/config.h
@@ -3,34 +3,38 @@
 namespace vwtiling {
 
 struct RGBColor {
-  double r;
-  double g;
-  double b;
+  float r;
+  float g;
+  float b;
 
-  RGBColor(double r_, double g_, double b_)
+  RGBColor(float r_, float g_, float b_)
     : r(r_)
     , g(g_)
     , b(b_) {}
 
   RGBColor(int r_, int g_, int b_)
-    : r(r_ / 255.0)
-    , g(g_ / 255.0)
-    , b(b_ / 255.0) {}
+    : r(r_ / 255.f)
+    , g(g_ / 255.f)
+    , b(b_ / 255.f) {}
 };
 
 struct Config {
-  int height = 30;
-  int pad = 26;
-  double corner_radius = 0; // TODO: Transparency is janky
+  Config();
 
-  int label_width = height;
+  int height;
+  int pad;
+  float corner_radius;
+  int label_width;
+  int outer_gap;
+  enum class Alignment {
+    Left,
+    Center,
+  } alignment;
 
   RGBColor background_color = RGBColor(208, 222, 207);
-  RGBColor highlight_color = RGBColor(117, 190, 195);
+  RGBColor highlight_color = RGBColor(0, 0, 0);
   RGBColor inactive_text_color = highlight_color;
   RGBColor active_text_color = background_color;
-
-  int outer_gap = 16;
 };
 
 } // namespace vwtiling
