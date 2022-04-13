47f27c199af59060cfed8a9b9fd01aa70aee0760
Jerry Yin <jerryyin@outlook.com>
2022-04-13 00:14:44 -0700

    Make config input validation stricter

--- src/config.cpp
+++ src/config.cpp
@@ -24,29 +24,76 @@ Config::Config()
   inactive_text_color = highlight_color;
 }
 
+static bool handle_bool(const char* value, bool* attr) {
+  if (strcmp(value, "true") == 0) {
+    *attr = true;
+  } else if (strcmp(value, "false") == 0) {
+    *attr = false;
+  } else {
+    return false; /* Error: unrecognized. */
+  }
+  return true;
+}
+
+static bool handle_color(const char* value, RGBColor* attr) {
+  if (strlen(value) == 7 && value[0] == '#') {
+    char* endptr = nullptr;
+    errno = 0;
+    RGBColor color = RGBColor(strtol(value + 1, &endptr, 16));
+    if (*endptr == '\0' && errno == 0) {
+      *attr = color;
+      return true;
+    }
+    errno = 0;
+  }
+  return false;
+}
+
+static bool handle_positive_int(const char* value, int* attr) {
+  char* endptr = nullptr;
+  errno = 0;
+  int x = strtol(value, &endptr, 10);
+  if (*endptr == '\0' && errno == 0 && x > 0) {
+    *attr = x;
+    return true;
+  }
+  errno = 0;
+  return false;
+}
+
+static bool handle_non_negative_int(const char* value, int* attr) {
+  char* endptr = nullptr;
+  errno = 0;
+  int x = strtol(value, &endptr, 10);
+  if (*endptr == '\0' && errno == 0 && x >= 0) {
+    *attr = x;
+    return true;
+  }
+  errno = 0;
+  return false;
+}
+
 int config_entry_handler(void* user, const char* section, const char* name,
                          const char* value) {
   Config* config = (Config*)user;
 
 #define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
 
-#define HANDLE_BOOL(attr)                                                                \
-  do {                                                                                   \
-    if (strcmp(value, "true") == 0) {                                                    \
-      attr = true;                                                                       \
-    } else if (strcmp(value, "false") == 0) {                                            \
-      attr = false;                                                                      \
-    } else {                                                                             \
-      return 0; /* Error: unrecognized. */                                               \
-    }                                                                                    \
-  } while (0)
-
   if (MATCH("geometry", "height")) {
-    config->height = atoi(value);
+    if (!handle_positive_int(value, &config->height)) {
+      return 0;
+    }
+
   } else if (MATCH("geometry", "pad")) {
-    config->pad = atoi(value);
+    if (!handle_non_negative_int(value, &config->pad)) {
+      return 0;
+    }
+
   } else if (MATCH("geometry", "label_width")) {
-    config->label_width = atoi(value);
+    if (!handle_positive_int(value, &config->label_width)) {
+      return 0;
+    }
+
   } else if (MATCH("geometry", "alignment")) {
     if (strcmp(value, "left") == 0) {
       config->alignment = Config::Alignment::Left;
@@ -57,40 +104,44 @@ int config_entry_handler(void* user, const char* section, const char* name,
     } else {
       return 0; // Error: unrecognized.
     }
+
   } else if (MATCH("geometry", "bottom")) {
-    HANDLE_BOOL(config->bottom);
+    handle_bool(value, &config->bottom);
+
   } else if (MATCH("colors", "background")) {
-    if (strlen(value) == 7 && value[0] == '#') {
-      config->background_color = strtol(value + 1, nullptr, 16);
-    } else {
+    if (!handle_color(value, &config->background_color)) {
       return 0; // Error: invalid colour.
     }
+
   } else if (MATCH("colors", "highlight")) {
-    if (strlen(value) == 7 && value[0] == '#') {
-      config->highlight_color = strtol(value + 1, nullptr, 16);
-    } else if (strcmp(value, "auto") == 0) {
+    if (strcmp(value, "auto") == 0) {
       get_accent_color(&config->highlight_color);
-    } else {
+    } else if (!handle_color(value, &config->highlight_color)) {
       return 0; // Error: invalid colour.
     }
+
   } else if (MATCH("colors", "active")) {
-    if (strlen(value) == 7 && value[0] == '#') {
-      config->active_text_color = strtol(value + 1, nullptr, 16);
-    } else {
+    if (!handle_color(value, &config->active_text_color)) {
       return 0; // Error: invalid colour.
     }
+
   } else if (MATCH("colors", "inactive")) {
-    if (strlen(value) == 7 && value[0] == '#') {
-      config->inactive_text_color = strtol(value + 1, nullptr, 16);
-    } else if (strcmp(value, "auto") == 0) {
+    if (strcmp(value, "auto") == 0) {
       get_accent_color(&config->inactive_text_color);
-    } else {
+    } else if (!handle_color(value, &config->inactive_text_color)) {
       return 0; // Error: invalid colour.
     }
+
   } else if (MATCH("keybinds", "window_switch")) {
-    HANDLE_BOOL(config->window_switch);
+    if (!handle_bool(value, &config->window_switch)) {
+      return 0;
+    }
+
   } else if (MATCH("keybinds", "wraparound")) {
-    HANDLE_BOOL(config->window_switch_wrap);
+    if (!handle_bool(value, &config->window_switch_wrap)) {
+      return 0;
+    }
+
   } else {
     return 0; /* unknown section/name, error */
   }
